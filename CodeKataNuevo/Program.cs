05dabba4687404c408259834c8cd3b3be7006192
frncisco lurbe <francisco.lurbe@gmail.com>
2012-06-19 13:50:03 +0200

    PRimer commit

---
+++ CodeKataNuevo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ Dado un ﬁchero de texto que contenga una secuencia de 0's y 1's determinar si la secuencia es 
+ * periódica y en caso de serlo extraer tanto el periodo como su longitud.
+
+Por ejemplo, dado el fichero con la secuencia 1101111011110111101111 el programa devolvería:
+
+La secuencia es periódica.
+Periodo: 11011
+Longitud: 5 
+ */
+namespace CodeKataSecuenciaBinaria
+{
+    class Program
+    {
+        
+        //{1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,};
+        static void Main(string[] args)
+        {
+            //Secuencia secuencia = new Secuencia("1101111011110111101111");
+            Secuencia secuencia = new Secuencia("0110111010");   
+                                                              
+        }        
+    }
+}
---
+++ CodeKataNuevo/Secuencia.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKataSecuenciaBinaria
+{
+    class Secuencia
+    {
+        private char[] entradaarray;
+        public string retorno { get; set; }
+        public Secuencia(string entrada)
+        {
+            entradaarray = new char[entrada.Length];
+            
+            entradaarray = entrada.ToCharArray();
+            string retornoStr = Secuenciar().ToString();
+            int retorno = Secuenciar();
+            char[] secuenciaarray = new char[retorno];           
+            
+            for (int i = 0; i < retorno; i++)
+            {
+                secuenciaarray[i]  = entradaarray[i];                
+            }
+            String str =new String(secuenciaarray);
+
+            Console.WriteLine("Longitud: " + retorno);
+            Console.WriteLine("Secuencia: " + str);
+            Console.Read();
+        }
+
+        private int Secuenciar()
+        {
+            int aux = entradaarray.Length / 2;
+            while (!SonIguales(0, aux) && aux > 1)
+            {
+                aux--;
+            }
+            return aux;
+        }        
+
+        private bool SonIguales(int ini, int fin)
+        {
+            int longitud = fin - ini;
+            if (longitud > 0)
+            { 
+                char[] entradaarrayIni = new char[fin - ini];
+                char[] entradaarrayFin = new char[fin - ini];
+                for (int i = ini; i < fin; i++)
+                {
+                    entradaarrayIni[i] = entradaarray[i];
+                    entradaarrayFin[i] = entradaarray[i+longitud];
+                }
+
+                for (int i = 0; i < longitud; i++)
+                {
+                    if (entradaarrayIni[i] != entradaarrayFin[i])
+                    { 
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool ContieneLosDos(int ini, int fin)
+        {
+            if (Contiene('0', ini, fin)) 
+                if (Contiene('1', ini, fin))
+                {
+                    return true;
+                }
+            return false;
+        }
+
+        private bool Contiene(char caracter,int ini,int fin)
+        { 
+            int pos = ini;
+            while (entradaarray[pos] != caracter && pos<fin)
+            {
+                pos++;
+                if (pos == fin - 1) return false;
+            }
+            return true;
+        }
+
+
+    }
+}
